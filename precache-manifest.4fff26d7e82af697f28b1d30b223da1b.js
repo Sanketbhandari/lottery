d88643f0df133347d24da97a050ae85734d3196c
Sanketbhandari <sanketbhandari22@gmail.com>
2022-10-06 18:16:56 +0530

    Updates

---
+++ precache-manifest.4fff26d7e82af697f28b1d30b223da1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb15653c079a8ffb261cd32ff7572df0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd5800f7e04ae5f48311",
+    "url": "/static/css/main.73a84d3d.chunk.css"
+  },
+  {
+    "revision": "941d69dbd2319fed9f24",
+    "url": "/static/js/2.f0cde87d.chunk.js"
+  },
+  {
+    "revision": "fd5800f7e04ae5f48311",
+    "url": "/static/js/main.3be3f8cd.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
