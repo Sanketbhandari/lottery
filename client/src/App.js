7c203a8ffea89e899b615d30bcaa2fa6bc9c7721
Sanketbhandari <sanketbhandari22@gmail.com>
2022-10-06 18:15:46 +0530

    updated final version

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from "react";
 import getWeb3 from "./getWeb3";
-import SimpleStorage from "./contracts/SimpleStorage.json";
+import Lottery from "./contracts/Lottery.json";
+
+import Manager from "./components/Manager";
+import Players from "./components/Players";
+import Intro from "./components/Intro";
 import "./App.css";
+import { Route, Link } from "react-router-dom";
 
 const App = () => {
   const [state, setState] = useState({
     web3: null,
     contract: null,
   });
-  const [storageValue, setStorageValue] = useState(null);
+  const [address, setAddress] = useState(null);
 
   useEffect(() => {
     const init = async () => {
@@ -16,12 +21,13 @@ const App = () => {
         const web3 = await getWeb3();
         const networkId = await web3.eth.net.getId();
 
-        const deployedNetwork = SimpleStorage.networks[networkId];
+        const deployedNetwork = Lottery.networks[networkId];
         console.log("Contract Address:", deployedNetwork.address);
         const instance = new web3.eth.Contract(
-          SimpleStorage.abi,
+          Lottery.abi,
           deployedNetwork && deployedNetwork.address
         );
+        setAddress(deployedNetwork.address);
         setState({ web3, contract: instance });
       } catch (error) {
         alert("Falied to load web3 or contract.");
@@ -31,19 +37,46 @@ const App = () => {
     init();
   }, []);
 
-  useEffect(() => {
-    async function getValue() {
-      const { contract } = state;
-      const value = await contract.methods.get().call();
-      setStorageValue(value);
-    }
-    state.contract && getValue();
-  }, [state.contract, state]);
-
   return (
-    <div className="App">
-      <div>The stored values is: {storageValue}</div>
-    </div>
+    <>
+      <nav className="navbar navbar-expand-lg navbar">
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to="/" className="nav-link navtext" aria-current="page">
+                  Lottery System
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/manager"
+                  className="nav-link navtext"
+                  aria-current="page"
+                >
+                  Manger
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/players" className="nav-link navtext">
+                  Player
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <Route exact path="/">
+        <Intro></Intro>
+      </Route>
+      <Route path="/manager">
+        <Manager state={state} />
+      </Route>
+      <Route path="/players">
+        <Players address={address} state={state} />
+      </Route>
+    </>
   );
 };
 export default App;
